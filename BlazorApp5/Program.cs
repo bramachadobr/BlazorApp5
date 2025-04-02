0d6ffb63fd5b9e7c0fa7139e2b190003ab72a147
Edivaldo Machado <edivaldomachado@gmail.com>
2025-04-02 18:16:47 -0300

    Implementacao de login

--- BlazorApp5/Program.cs
+++ BlazorApp5/Program.cs
@@ -1,5 +1,6 @@
 using BlazorApp5.Components;
 using BlazorApp5.Servicos;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp5
@@ -20,6 +21,11 @@ namespace BlazorApp5
             //SqlExpressConnectionString3
             //SqlExpressConnectionStringDocker
 
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            builder.Services.AddScoped<CustomAuthenticationStateProvider>(); // Para poder chamar login/logout
+
+
 
             builder.Services.AddScoped<INFService, NFService>();
             builder.Services.AddScoped<IFornecedorService, FornecedorService>();
@@ -35,6 +41,13 @@ namespace BlazorApp5
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //builder.Services.AddOidcAuthentication(options =>
+            //{
+            //    // Configure your authentication provider options here.
+            //    // For more information, see https://aka.ms/blazor-standalone-auth
+            //    builder.Configuration.Bind("Local", options.ProviderOptions);
+            //});
 
             app.UseHttpsRedirection();
 
---
+++ BlazorApp5/Servicos/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+    private ClaimsPrincipal _currentUser = null;
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        return Task.FromResult(new AuthenticationState(_currentUser ?? _anonymous));
+    }
+
+    public void Login(string username)
+    {
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, username),
+            new Claim(ClaimTypes.Role, "Admin") // ou "User", se quiser testar papéis diferentes
+        }, "Fake authentication");
+
+        _currentUser = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+
+    public void Logout()
+    {
+        _currentUser = _anonymous;
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+}
